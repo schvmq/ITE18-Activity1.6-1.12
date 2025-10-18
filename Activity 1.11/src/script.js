4a276a42284209872d82b768a24370d07f961dcd
schvmq <smaejaban@gmail.com>
2025-10-18 14:16:47 +0800

    script.js act 11

--- Activity 1.11/src/script.js
+++ Activity 1.11/src/script.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 
 /**
  * Base
@@ -19,15 +21,72 @@ const scene = new THREE.Scene()
  */
 const textureLoader = new THREE.TextureLoader()
 
+
+/**
+* Fonts
+*/
+const fontLoader = new FontLoader()
+fontLoader.load(
+    '/fonts/helvetiker_regular.typeface.json',
+    (font) =>
+    {
+    const textGeometry = new TextGeometry(
+    'apakaangas',
+    {
+    font: font,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 3,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 1
+    }
+    )
+    textGeometry.computeBoundingBox()
+    console.log(textGeometry.boundingBox)
+    //textGeometry.translate(
+    //    - textGeometry.boundingBox.max.x * 0.5,
+    //    - textGeometry.boundingBox.max.y * 0.5,
+    //    - textGeometry.boundingBox.max.z * 0.5 
+    //)
+    textGeometry.center()
+
+    const matcapTexture = textureLoader.load('/textures/matcaps/1.png')
+    const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+    const text = new THREE.Mesh(textGeometry, material)
+    scene.add(text)
+
+    
+    const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+    //const donutMaterial = new THREE.MeshMatcapMaterial({ matcap:matcapTexture })
+    for(let i = 0; i < 100; i++)
+    {
+        const donut = new THREE.Mesh(donutGeometry, material)
+        donut.position.x = (Math.random() - 0.5) * 10
+        donut.position.y = (Math.random() - 0.5) * 10
+        donut.position.z = (Math.random() - 0.5) * 10
+        donut.rotation.x = Math.random() * Math.PI
+        donut.rotation.y = Math.random() * Math.PI
+        const scale = Math.random()
+        donut.scale.set(scale, scale, scale)
+        scene.add(donut)
+    }
+    }
+)
+
+
+
 /**
  * Object
- */
+ 
 const cube = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1),
     new THREE.MeshBasicMaterial()
 )
 
-scene.add(cube)
+scene.add(cube)*/
 
 /**
  * Sizes
